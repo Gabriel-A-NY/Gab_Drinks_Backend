97bae91a1cfc63f6d4aa16bd0a02a96613407ea3
gabriel.arocha@edu.tbz.ch <gabriel.arocha@edu.tbz.ch>
2024-08-21 08:59:39 +0200

    implement @preauthorize to complete security feat. in backend

--- src/main/java/ch/noseryoung/domain/drink/DrinkController.java
+++ src/main/java/ch/noseryoung/domain/drink/DrinkController.java
@@ -3,6 +3,7 @@ package ch.noseryoung.domain.drink;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.management.InstanceAlreadyExistsException;
@@ -21,32 +22,34 @@ public class DrinkController {
 
     @CrossOrigin(origins = "http://localhost:5175")
     @GetMapping("/all")
+    @PreAuthorize("hasAnyAuthority('GET')")
     public List<Drink> showAll() {
         return drinkService.findAll();
     }
 
     @CrossOrigin(origins = "http://localhost:5175")
     @GetMapping("/{product_id}")
+    @PreAuthorize("hasAnyAuthority('GET')")
     public ResponseEntity<Drink> getById(@PathVariable("product_id") int id) throws InstanceNotFoundException {
         return ResponseEntity.ok().body(drinkService.getById(id));
     }
 
     @CrossOrigin(origins = "http://localhost:5175")
-
+    @PreAuthorize("hasAnyAuthority('POST')")
     @PostMapping("/add")
     public ResponseEntity<Drink> createDrink(@RequestBody Drink newDrink) throws InstanceAlreadyExistsException {
         return ResponseEntity.status(201).body(drinkService.addDrink(newDrink));
     }
 
     @CrossOrigin(origins = "http://localhost:5175")
-
+    @PreAuthorize("hasAnyAuthority('PUT')")
     @PutMapping(value = "/{product_id}")
     public ResponseEntity<Drink> updateDrink(@PathVariable("product_id") int productId, @RequestBody Drink drink) throws InstanceNotFoundException, InstanceAlreadyExistsException {
         return ResponseEntity.status(200).body(drinkService.updateById(productId, drink));
     }
 
     @CrossOrigin(origins = "http://localhost:5175")
-
+    @PreAuthorize("hasAnyAuthority('DELETE')")
     @DeleteMapping("{product_id}")
     public String deleteDrink(@PathVariable("product_id") int productId) throws InstanceNotFoundException {
         drinkService.deleteDrink(productId);
@@ -54,9 +57,8 @@ public class DrinkController {
     }
 
     @CrossOrigin(origins = "http://localhost:5175")
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("error" + e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("err" + e.getMessage());
     }
 }
